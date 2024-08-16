b07bdd3c83f904ce0dd38c56432860772084b179
Daniel W <deniel98045@gmail.com>
2024-08-16 20:55:08 +0200

    added filters

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrl: './filters.component.css',
+})
+export class FiltersComponent {
+  @Output() showCategory = new EventEmitter<string>();
+  categories = ['shoes', 'pants'];
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+  }
+}
--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html',
-  styleUrl: './products-header.component.css'
+  styleUrl: './products-header.component.css',
 })
 export class ProductsHeaderComponent {
+  @Output() columnsCountChange = new EventEmitter<number>();
 
+  sort = 'desc';
+  itemsCount = 12;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+
+  onItemsUpdated(newCount: number) {
+    this.itemsCount = newCount;
+  }
+
+  onColumnUpdated(newColumnCount: number): void {
+    this.columnsCountChange.emit(newColumnCount);
+  }
 }
